88c67e040643964db29487acd036a1c967b102e9
YOUSEF-IS-HERE <40815405+YOUSEF-IS-HERE@users.noreply.github.com>
2019-05-16 07:11:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,8 @@
 const Discord = require('discord.js');
 
-const Util = require('discord.js');
-//n3k4a جميع الحقوق معفوظه لي //n3k4a جميع الحقوق معفوظه لي 
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');//n3k4a جميع الحقوق معفوظه لي 
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-//n3k4a جميع الحقوق معفوظه لي 
-const queue = new Map();
-
-const ytdl = require('ytdl-core');//n3k4a جميع الحقوق معفوظه لي 
-
-const fs = require('fs');
-//n3k4a جميع الحقوق معفوظه لي 
-const gif = require("gif-search");
-
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "=";//n3k4a جميع الحقوق معفوظه لي //n3k4a جميع الحقوق معفوظه لي //n3k4a جميع الحقوق معفوظه لي //n3k4a جميع الحقوق معفوظه لي 
-/////////////////////////
-////////////////////////
+const prefix = "=";
 const music = new Music(client, {
     prefix: ".", // Prefix for the commands.
     youtubeKey: 'AIzaSyApvbcgvYRGulf1I1Ffjfhr2K-S6TX0e9w',
