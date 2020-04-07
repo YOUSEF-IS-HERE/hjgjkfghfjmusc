c2a0d9d3d0242f599d0583587caefeec186bf4af
YOUSEF-IS-HERE <40815405+YOUSEF-IS-HERE@users.noreply.github.com>
2020-04-08 01:45:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,30 +2,61 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "=";
-const music = new Music(client, {
-    prefix: ".", // Prefix for the commands.
-    youtubeKey: 'AIzaSyApvbcgvYRGulf1I1Ffjfhr2K-S6TX0e9w',
-    global: false,            // Non-server-specific queues.
-    maxQueueSize: 50,        // Maximum queue size of 25.
-    playCmd: 'p',        // Sets the name for the 'play' command.
-    playAlts: ["play", 'paly'],
-    volumeCmd: 'vol',     // Sets the name for the 'volume' command.
-    thumbnailType: 'high',
-    leaveCmd: 'stop',      // Sets the name for the 'leave' command.
-    anyoneCanSkip: true,
-    disableLoop: false,
-    searchCmd: 'search',
-    requesterName: true,
-    inlineEmbeds: true,	 
-    queueCmd: 'q',
-    queueAlts: ['queue', 'queueue'],
-    pauseCmd: 'pause',
-    resumeCmd: 'resume',
-    skipCmd: 's',
-    skipAlts: ["skip", "skipp"],
-    loopCmd: 'loop',
-    enableQueueStat: true,
-  });
+const prefix = "$";
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "كيفك بخير") {
+    LGD_GHASSAN.channel.send("تمام");
+  }
+});
 
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "تمام") {
+    LGD_GHASSAN.channel.send("اش اخبارك");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "اش اخبارك") {
+    LGD_GHASSAN.channel.send("كويس");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "كويس") {
+    LGD_GHASSAN.channel.send("اكيد");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "اكيد") {
+    LGD_GHASSAN.channel.send("ايوا");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "ايوا") {
+    LGD_GHASSAN.channel.send("اوك");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "اوك") {
+    LGD_GHASSAN.channel.send("كيفك بخير");
+  }
+});
+
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "$c") {
+    LGD_GHASSAN.channel.send("#credit");
+  }
+});
+client.on("message",LGD_GHASSAN => {
+  if (LGD_GHASSAN.author.bot) return;
+  if (LGD_GHASSAN.content === "$d") {
+    LGD_GHASSAN.channel.send("#daily");
+  }
+});
 client.login(process.env.BOT_TOKEN);
